45922ed4883181ecf1a5f3bca74f4f895e461d74
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2022-03-19 12:56:20 -0600

    Update

--- Script.R
+++ Script.R
@@ -2,24 +2,20 @@ library(e1071)
 library(caret)
 
 #Calculo de percentiles
+#base de datos a utilzar
 db<-read.csv('train.csv')
+#Encontramos los percentiles
 percentil <- quantile(db$SalePrice)
-
-
 #Percentiles
 estado<-c('Estado')
 db$Estado<-estado
-
-
-#Economica=0
-#Intermedia=1
-#Cara=2
 db <- within(db, Estado[SalePrice<=129975] <- 'Economica')
 
 db$Estado[(db$SalePrice>129975 & db$SalePrice<=163000)] <- 'Intermedia'
 db$Estado[db$SalePrice>163000] <- 'Cara'
 
 #Bayes 
+#Usamos el 70% de datos
 porcentaje<-0.7
 set.seed(1234)
 
@@ -39,10 +35,14 @@ test$GarageArea<-as.numeric(test$GarageArea)
 test$YearRemodAdd<-as.numeric(test$YearRemodAdd)
 test$SalePrice<-as.numeric(test$SalePrice)
 test$LotArea<-as.numeric(test$LotArea)
+
+
+
+
 table(predBayes)
 table(test$Estado)
 predBayes<-as.factor(predBayes)
-str(test$Estado)
+
 predBayes<-predict(modelo, newdata = test[,c("GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","GarageArea","YearRemodAdd", "SalePrice","LotArea")])
 cm<-caret::confusionMatrix(as.factor(predBayes),as.factor(test$Estado))
 cm
@@ -50,31 +50,24 @@ cm
 #Cross Validation
 #GarageYrBlt  MasVnrArea LotFrontage /PoolQC Fence MiscFeature FireplaceQu Alley
 #Cambiamos los NA
-# 
+
 train[c("GarageYrBlt")][is.na(train[c("GarageYrBlt")])] <- 1979
 train[c("MasVnrArea")][is.na(train[c("MasVnrArea")])] <- 0
 train[c("LotFrontage")][is.na(train[c("LotFrontage")])] <- 68.00
 
-# train[is.na(train)] <- 0
-# #summary(train)
-# 
-# #train <- subset( train, select = -c('PoolQC') )
-# # 
-# drop <- c("PoolQC","Fence","MiscFeature","FireplaceQu","Alley")
-# train = train[,!(names(train) %in% drop)]
-# 
-train <- train[ , ! names(train) %in% c("PoolQC","Fence","MiscFeature","FireplaceQu","Alley","MasVnrType","BsmtQual","BsmtCond","BsmtExposure","BsmtFinType1","BsmtFinType2","GarageType","GarageFinish","GarageQual",'GarageCond')]
-# is.na(train)
-#train[, colSums(is.na(train)) != nrow(train)]
-str(train$MasVnrType)
-sum(is.na(train))
-#is.na(train[,1])
-View(train)
+sum(is.na(train$BsmtExposure))
+
+names(which(colSums(is.na(train))>0)) 
+
+
+sum(is.na(train$GrLivArea))
+
+
 ct<-trainControl(method = "cv",train[,c("GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","GarageArea","YearRemodAdd", "SalePrice","LotArea")],number=10, verboseIter=T)
-modeloCaret<-train(Estado~ .,data=train,method="nb",trControl = ct,na.action=na.omit)
+modeloCaret<-train(Estado~ .,data=train[,c("GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","GarageArea","YearRemodAdd", "SalePrice","LotArea","Estado")],method="nb",trControl = ct)
 
 prediccionCaret<-predict(modeloCaret,newdata = test[,c("GrLivArea","YearBuilt","BsmtUnfSF","TotalBsmtSF","GarageArea","YearRemodAdd", "SalePrice","LotArea")])
-caret::confusionMatrix(prediccionCaret,test$Estado)
+caret::confusionMatrix(prediccionCaret,as.factor(test$Estado))
 
 
 
